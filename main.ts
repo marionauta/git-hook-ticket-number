1f3d504635dfe38289d71b10f8f0c81e5ed66abc
Mario Nachbaur <marionauta@users.noreply.github.com>
2025-02-03 17:41:34 +0100

    feat: add configuration via `git config`

--- main.ts
+++ main.ts
@@ -4,6 +4,42 @@ import { exit } from "node:process";
 type BracketStyle = "none" | "round" | "square" | "curly";
 type ContextPosition = "start" | "before_colon" | "after_colon";
 
+interface Configuration {
+  bracketStyle: BracketStyle;
+  contextPosition: ContextPosition;
+}
+
+async function readConfiguration(): Promise<Configuration> {
+  let configuration: Configuration = {
+    bracketStyle: "square",
+    contextPosition: "after_colon",
+  };
+
+  const bracketStyleCmd = await $`git config ghtn.bracketStyle`
+    .quiet()
+    .nothrow();
+  if (bracketStyleCmd.exitCode) {
+    console.warn(bracketStyleCmd.stderr.toString().trimEnd());
+    return configuration;
+  }
+  const bracketStyle = bracketStyleCmd.text().trimEnd();
+  if (bracketStyle) {
+    configuration.bracketStyle = bracketStyle as BracketStyle;
+  }
+  const contextPositionCmd = await $`git config ghtn.contextPosition`
+    .quiet()
+    .nothrow();
+  if (contextPositionCmd.exitCode) {
+    console.warn(contextPositionCmd.stderr.toString().trimEnd());
+    return configuration;
+  }
+  const contextPosition = contextPositionCmd.text().trimEnd();
+  if (contextPosition) {
+    configuration.contextPosition = contextPosition as ContextPosition;
+  }
+  return configuration;
+}
+
 async function getBranchName(): Promise<string | undefined> {
   const branchNameCmd = await $`git branch --show-current`.quiet().nothrow();
   if (branchNameCmd.exitCode) {
@@ -19,23 +55,37 @@ function getTicketNumber(branchName: string): string | undefined {
   return matches.at(0)?.toUpperCase();
 }
 
-function bracketize(commit: string, bracketStyle: BracketStyle): string {
+function bracketize(line: string, bracketStyle: BracketStyle): string {
   switch (bracketStyle) {
-    case "none": return commit;
-    case "round": return `(${commit})`;
-    case "square": return `[${commit}]`;
-    case "curly": return `{${commit}}`;
+    case "round":
+      return `(${line})`;
+    case "square":
+      return `[${line}]`;
+    case "curly":
+      return `{${line}}`;
+    default:
+      return line;
   }
 }
 
-function apply(ticketNumber: string, commit: string, position: ContextPosition, bracketStyle: BracketStyle): string {
+function apply(
+  ticketNumber: string,
+  commit: string,
+  position: ContextPosition,
+  bracketStyle: BracketStyle,
+): string {
   const context = bracketize(ticketNumber, bracketStyle);
   if (position === "start") {
     return `${context} ${commit}`;
   }
-  const colonIndex = commit.indexOf(":") + (position === "before_colon" ? 0 : 1);
+  const colonIndex =
+    commit.indexOf(":") + (position === "before_colon" ? 0 : 1);
   if (colonIndex < 0) return commit;
-  return commit.slice(0, colonIndex).trimEnd() + ` ${context}${position === "after_colon" ? " " : ""}` + commit.slice(colonIndex).trimStart();
+  return (
+    commit.slice(0, colonIndex).trimEnd() +
+    ` ${context}${position === "after_colon" ? " " : ""}` +
+    commit.slice(colonIndex).trimStart()
+  );
 }
 
 const argv = Bun.argv.slice(2); // skip bun & program name
@@ -44,7 +94,9 @@ switch (command) {
   case undefined:
     break;
   case "install":
-    const hooksDirCmd = await $`git rev-parse --git-path hooks`.quiet().nothrow();
+    const hooksDirCmd = await $`git rev-parse --git-path hooks`
+      .quiet()
+      .nothrow();
     if (hooksDirCmd.exitCode) {
       console.warn(hooksDirCmd.stderr.toString().trimEnd());
       exit(hooksDirCmd.exitCode);
@@ -63,7 +115,14 @@ switch (command) {
     const ticketNumber = getTicketNumber(branchName);
     if (!ticketNumber) exit(0);
     if (line.toUpperCase().includes(ticketNumber)) exit(0);
-    const edited = apply(ticketNumber, line, "after_colon", "square");
+    const configuration = await readConfiguration();
+    if (!configuration) exit(0);
+    const edited = apply(
+      ticketNumber,
+      line,
+      configuration.contextPosition,
+      configuration.bracketStyle,
+    );
     const editedMessage = [edited, ...message.slice(1)].join("\n");
     await Bun.write(command, editedMessage);
     exit(0);
